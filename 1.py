62ba5071438baf7f40b19db5a220063cb8f86d73
my <hch728@nate.com>
2022-04-09 17:27:32 +0900

    적용 바랍니다.

--- 1.py
+++ 1.py
@@ -41,103 +41,63 @@ print(training_data.head())
 print(target.head())
 
 value_data = training_data[['age', 'fare']]
-
 print(value_data.head())
 
 
 
 scaler = StandardScaler()
-
 scaled_data = scaler.fit_transform(value_data)
-
 value_data = pd.DataFrame(scaled_data, columns=value_data.columns)
-
 print(value_data.describe())
 
 
 
 training_data.info()
-
 training_data.drop(['age', 'fare'], axis=1, inplace=True)
-
 training_data.pclass.unique()
 
-
-
 onehot_data = pd.get_dummies(training_data,
-
             columns=training_data.columns)
 
-
-
 print(onehot_data.head())
-
 print(onehot_data.info())
 
-
-
 training_data = pd.concat((onehot_data, value_data), axis=1)
-
 print(training_data.info())
 
-
-
-
-
 X_train, X_test, Y_train, Y_test = train_test_split(
-
     training_data, target, test_size=0.2)
-
 print(X_train.shape, Y_train.shape)
-
 print(X_test.shape, Y_test.shape)
 
 
 
 model = Sequential()
-
 model.add(Dense(128, input_dim=34, activation='relu'))
-
 model.add(Dropout(0.02))
-
 model.add(Dense(256, activation='relu'))
-
 model.add(Dropout(0.02))
-
 model.add(Dense(512, activation='relu'))
-
 model.add(Dropout(0.02))
 
 model.add(Dense(256, activation='relu'))
-
 model.add(Dropout(0.02))
-
 model.add(Dense(128, activation='relu'))
-
 model.add(Dropout(0.02))
-
 model.add(Dense(64, activation='relu'))
-
 model.add(Dropout(0.02))
-
 model.add(Dense(32, activation='relu'))
-
 model.add(Dropout(0.02))
-
 model.add(Dense(1, activation='sigmoid'))
-
 model.summary()
 
 
 
 model.compile(loss='mse', optimizer='adam',
-
               metrics=['binary_accuracy'])
 
 fit_hist = model.fit(
-
     X_train, Y_train, batch_size=50, epochs=10,
-
     validation_split=0.2, verbose=1)
 
 
@@ -145,18 +105,11 @@ fit_hist = model.fit(
 
 
 plt.plot(fit_hist.history['binary_accuracy'])
-
 plt.plot(fit_hist.history['val_binary_accuracy'])
-
 plt.show()
 
 
 
 score = model.evaluate(X_test, Y_test, verbose=0)
-
 print('loss', score[0])
-
 print('accuracy', score[1])
-
-
-
