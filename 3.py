695eeffa6e5334a95e1e637be912da99c3996562
my <hch728@nate.com>
2022-04-09 17:06:31 +0900

    적용 바랍니다.

--- 3.py
+++ 3.py
@@ -29,7 +29,7 @@ clean_data = clean_data.dropna()
 
 keep = column_name.pop()
 
-print(keep)
+# print(keep)
 
 training_data = clean_data[column_name]
 
@@ -43,9 +43,9 @@ target = clean_data[[keep]]
 
 # print(target.head())
 
-# print(target['HeartDisease'].sum())
+# print(target['h'].sum())
 
-# print(target['HeartDisease'].mean())
+# print(target['h'].mean())
 
 # print(scaled_data.describe().T)
 
@@ -57,25 +57,27 @@ target = clean_data[[keep]]
 
 from sklearn.preprocessing import StandardScaler
 #
-# scaler = StandardScaler()
+scaler = StandardScaler()
 #
-# scaled_data = scaler.fit_transform(training_data)
+scaled_data = scaler.fit_transform(training_data)
+# print(scaled_data)
 #
-# # print(type(scaled_data))
+# print(type(scaled_data))
 #
-# scaled_data = pd.DataFrame(scaled_data, columns=column_name)
+scaled_data = pd.DataFrame(scaled_data, columns=column_name)
 # #
-# # print(scaled_data.head())
+# print(scaled_data.head())
 #
-# # print(scaled_data)
+# print(scaled_data)
 #
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 #
-# X_train, X_test, Y_train, Y_test = train_test_split(
+X_train, X_test, Y_train, Y_test = train_test_split(
 #
-#     scaled_data, target, test_size=0.30)
+    scaled_data, target, test_size=0.30)
 
 # print(X_train)
+# print(Y_train)
 #
 # print('X_train :', X_train.shape)
 
@@ -86,41 +88,30 @@ from sklearn.preprocessing import StandardScaler
 # print('Y_test :', Y_test.shape)
 #
 #
-#
-#
-#
-# model = Sequential()
-#
-# model.add(Dense(512, input_dim=13, activation='relu'))
-#
-# model.add(Dropout(0.25))
-#
-# model.add(Dense(256, activation='relu'))
-#
-# model.add(Dropout(0.2))
-#
-# model.add(Dense(128, activation='relu'))
-#
-# model.add(Dropout(0.1))
-#
-# model.add(Dense(1, activation='sigmoid'))
-#
-# model.summary()
-#
-#
-#
-#
-#
-# model.compile(loss='mse', optimizer='adam',
-#
-#               metrics=['binary_accuracy'])
-#
-#
-#
-# fit_hist = model.fit(X_train, Y_train,
-#
-#             batch_size=50, epochs=10,
-#
-#             validation_split=0.2, verbose=1)
-#
+##
+model = Sequential()
+model.add(Dense(512, input_dim=7, activation='relu'))   #7는 X_train의 열의 개수이여야 한다. 마지막 keep를 빼면 7이다.
+model.add(Dropout(0.25))
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
 
+model.compile(loss='mse', optimizer='adam',
+              metrics=['binary_accuracy'])
+
+fit_hist = model.fit(X_train, Y_train,
+            batch_size=50, epochs=10,
+            validation_split=0.2, verbose=1)
+
+
+plt.plot(fit_hist.history['binary_accuracy'])
+plt.plot(fit_hist.history['val_binary_accuracy'])
+plt.show()
+
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Keras DNN model loss :', score[0])
+print('Keras DNN model accuracy :', score[1])
