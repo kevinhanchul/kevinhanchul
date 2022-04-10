e2d53884d14af451cc42884c925e5cb70698270d
my <hch728@nate.com>
2022-04-10 21:50:23 +0900

    적용 바랍니다.

--- ex1.py
+++ ex1.py
@@ -3,11 +3,17 @@ import pandas as pd
 a = [1,2,3]
 a = pd.DataFrame(a)
 
-print(a)
-print(a.shape)
+# print(a)
+# print(a.shape)
 
 #위의 리스트와 타이틀만 틀리고 똑같음
 a = {1:[1,2,3]}
+a = pd.DataFrame(a)
+
+# print(a)
+# print(a.shape)
+
+a = [[1,2],[3,4]]
 a = pd.DataFrame(a)
 
 print(a)
