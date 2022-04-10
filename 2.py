b53a6348e9edf6ae33df283872899bb7eb509e32
my <hch728@nate.com>
2022-04-10 21:05:49 +0900

    적용 바랍니다.

---
+++ 2.py
@@ -0,0 +1,16 @@
+#마트 장보기 프로젝트 (클래스 사용)
+
+class Mart:
+    def __init__(self, no, item, price, cnt):
+        self.no = no
+        self.item = item
+        self.price = price
+        self.cnt = cnt
+
+    def __str__(self):
+        return "{}{}{}{}".format(self.no, self.item, self.price, self.cnt)
+
+Mart1 = [0,1,2,3]
+
+Mart1[0] = Mart(1, '아이템', 1500, 3)
+print(Mart1[0])
