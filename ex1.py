74bee03b1e245cd274b778723bc979e15b04cada
hch <hch728@nate.com>
2022-04-11 12:17:19 +0900

    학원에서 변경했습니다.

--- ex1.py
+++ ex1.py
@@ -1,20 +1,2 @@
-import pandas as pd
-
-a = [1,2,3]
-a = pd.DataFrame(a)
-
-# print(a)
-# print(a.shape)
-
-#위의 리스트와 타이틀만 틀리고 똑같음
-a = {1:[1,2,3]}
-a = pd.DataFrame(a)
-
-# print(a)
-# print(a.shape)
-
-a = [[1,2],[3,4]]
-a = pd.DataFrame(a)
-
-print(a)
-print(a.shape)
+a = 'abc'
+print({}.format(a))
