eb5cf8ee3a102cdb69ffdf456c1da74fd5e69e0c
hch <hch728@nate.com>
2022-04-11 17:42:18 +0900

    변경했습니다.

--- 1.py
+++ 1.py
@@ -1,35 +1,51 @@
-import requests
+# 그림 맞추기인가?
 
-from bs4 import BeautifulSoup
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import datasets
+from keras.utils import np_utils
 
-​
+(X_train, Y_train), (X_test, Y_test) = datasets.mnist.load_data()
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
 
-# url = "https://comic.naver.com/webtoon/genre?genre=omnibus"
+my_sample = np.random.randint(60000)
+plt.imshow(X_train[my_sample], cmap='gray')
+plt.show()
+print(Y_train[my_sample])
+print(X_train[my_sample])
 
-url = "http://localhost:8080/aaa.html"
+y_train = np_utils.to_categorical(Y_train)
+y_test = np_utils.to_categorical(Y_test)
+print(Y_train[5000])
+print(y_train[5000])
 
-​
+x_train = X_train.reshape(-1, 28 * 28)
+x_test = X_test.reshape(-1, 28 * 28)
+x_train = x_train / 255
+x_test = x_test / 255
+print(x_train.shape)
 
-​
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+print(model.summary())
 
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
+fit_hist = model.fit(x_train, y_train, batch_size=128,
+    epochs=15, validation_split=0.2, verbose=1)
 
-res = requests.get(url, headers=headers)
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Final test set accurecy :', score[1])
 
-​
+plt.plot(fit_hist.history['accuracy'])
+plt.plot(fit_hist.history['val_accuracy'])
+plt.show()
 
-res.raise_for_status()
+my_sample = np.random.randint(10000)
+plt.imshow(X_test[my_sample], cmap='gray')
+print(Y_test[my_sample])
+pred = model.predict(x_test[my_sample].reshape(-1, 28 * 28))
+print(pred)
+print(np.argmax(pred))
 
-res.encoding='UTF-8'
-
-​
-
-soup = BeautifulSoup(res.text, 'lxml')
-
-​
-
-ab = soup.find_all('li')
-
-​
-
-print(ab)
