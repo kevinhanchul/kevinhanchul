6e41c2ff5364877ccfed57200c0dad6fb785e8eb
my <hch728@nate.com>
2022-04-17 13:14:33 +0900

    적용 바랍니다.

--- 1.py
+++ 1.py
@@ -1,25 +1,25 @@
-#네이버 영화 리뷰
+<style>
+    .div1{
+        height: 250px; width: 100px;
+        border: 1px solid black;
+    }
+    .ul1{
+        height: 100px; width: 100px;
+        border: 1px solid black;
+    }
+</style>
 
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2022'
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-
-
-a = BeautifulSoup(res.text, "lxml")
-# a = a.find('ul',{'class':'directory_list'}).a['href']
-
-# print(a)
-# /movie/bi/mi/basic.naver?code=176037
-
-a1 = a.findAll('ul',{'class':'directory_list'})
-
-for i in a1:
-    k = i
-
-for i in k:
-    if i.find('a')!=-1:
-        print(i.find('a')['href'][-6:])
+<html>
+    <div class="div1">
+        <ul class="ul1">
+            <li class="li1">abc1</li>
+            <li class="li2">abc2</li>
+            <li class="li3">abc3</li>
+        </ul>
+        <ul class="ul2">
+            <li class="li4">abc1</li>
+            <li class="li5">abc2</li>
+            <li class="li6">abc3</li>
+        </ul>
+    </div>
+</html>
