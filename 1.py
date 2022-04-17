e7424601d5f859ce250be099f8cea12da078aec0
kevinhanchul <97497282+kevinhanchul@users.noreply.github.com>
2022-04-17 13:18:18 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,68 +1,25 @@
-#티셔츠 모양 확인
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
-from tensorflow.keras import datasets
-from keras.utils import np_utils
+<style>
+    .div1{
+        height: 250px; width: 100px;
+        border: 1px solid black;
+    }
+    .ul1{
+        height: 100px; width: 100px;
+        border: 1px solid black;
+    }
+</style>
 
-(X_train, Y_train), (X_test, Y_test) = datasets.fashion_mnist.load_data()
-print(X_train.shape, Y_train.shape)
-print(X_test.shape, Y_test.shape)
-
-label = ['T-shirt', 'trouser', 'pullover', 'dress', 'coat',
-         'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
-
-my_sample = np.random.randint(60000)
-plt.imshow(X_train[my_sample], cmap='gray')
-plt.show()
-print(Y_train[my_sample])
-print(X_train[my_sample])
-
-y_train = np_utils.to_categorical(Y_train)
-y_test = np_utils.to_categorical(Y_test)
-print(Y_train[5000])
-print(y_train[5000])
-
-x_train = X_train / 255
-x_test = X_test / 255
-x_train = x_train.reshape(60000, 28, 28, 1)
-print(x_train.shape)
-x_test = x_test.reshape(10000, 28, 28, 1)
-print(x_train.shape)
-print(x_train[0])
-
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-        input_shape=(28, 28, 1), padding='same',
-        activation='relu'))
-model.add(MaxPool2D(padding='same', pool_size=(2, 2)))
-model.add(Conv2D(32, kernel_size=(3, 3),
-        padding='same', activation='relu'))
-model.add(MaxPool2D(padding='same', pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dense(10, activation='softmax'))
-
-model.compile(optimizer='adam', loss='categorical_crossentropy',
-              metrics=['accuracy'])
-print(model.summary())
-
-fit_hist = model.fit(x_train, y_train, batch_size=128,
-    epochs=20, validation_split=0.2, verbose=1)
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Final test set accurecy :', score[1])
-
-plt.plot(fit_hist.history['accuracy'])
-plt.plot(fit_hist.history['val_accuracy'])
-plt.show()
-
-my_sample = np.random.randint(10000)
-plt.imshow(X_test[my_sample], cmap='gray')
-print(Y_test[my_sample])
-print(label[Y_test[my_sample]])
-pred = model.predict(x_test[my_sample].reshape(-1, 28, 28, 1))
-print(pred)
-print(label[np.argmax(pred)])
-
+<html>
+    <div class="div1">
+        <ul class="ul1">
+            <li class="li1">abc1</li>
+            <li class="li2">abc2</li>
+            <li class="li3">abc3</li>
+        </ul>
+        <ul class="ul2">
+            <li class="li4">abc1</li>
+            <li class="li5">abc2</li>
+            <li class="li6">abc3</li>
+        </ul>
+    </div>
+</html>
